6b78d22bf117823ca525430cc8a0037d4e4f2446
Rohan Baisantry <rohanbaisantry7@gmail.com>
2018-02-26 13:14:31 +0530

    Update LinearRegression_fromScratch_usingGradientDescent.py

--- LinearRegression_fromScratch_usingGradientDescent.py
+++ LinearRegression_fromScratch_usingGradientDescent.py
@@ -116,6 +116,6 @@ ax.set_ylabel('Y Label')
 ax.set_zlabel('Z Label')
 
 plt.show()
-np.savetxt("foo.csv", XYZ, delimiter=',', comments="")
+np.savetxt("visualize.csv", XYZ, delimiter=',', comments="")
 
 # END
